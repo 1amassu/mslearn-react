449900f44c244355bd04e8bfa84e23009805853e
fadeee <fadee.dev@gmail.com>
2021-05-20 18:46:57 +1000

    first app

---
+++ code/0-starter/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <article>
+            <h1>Recipe Manager</h1>
+            <h2> Recipe #1</h2>
+        </article>
+    )
+}
+
+export default App;
