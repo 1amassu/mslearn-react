ca4fbcf94d1a520eaab97a5a66bf93f2da843dbe
fadeee <fadee.dev@gmail.com>
2021-05-21 12:10:53 +1000

    final

--- code/0-starter/src/App.jsx
+++ code/0-starter/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import RecipeTitle from './RecipeTitle'
+import './index.css'
 
 function App() {
     return (
         <article>
             <h1>Recipe Manager</h1>
-            <h2> Recipe #1</h2>
+            <RecipeTitle />
         </article>
     )
 }
